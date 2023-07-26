ab2a16d4e928b8be2f6248263c42ae097b103c8b
jstarita <jcstarita1@up.edu.ph>
2023-07-26 14:03:08 +0800

    add mobile controls

--- src/navigation.js
+++ src/navigation.js
@@ -17,3 +17,18 @@ AFRAME.registerComponent('url-on-click', {
         });
     }
 });
+
+AFRAME.registerComponent('detect-ua', {
+    init: function () {
+      var el = this.el;
+
+      if (AFRAME.utils.device.isMobile()) {
+        // Add appropriate movement controls for mobile devices (touchscreen)
+        el.setAttribute('movement-controls', 'controls: keyboard, nipple');
+        el.setAttribute('nipple-controls', 'mode: static; lookJoystickEnabled: false; moveJoystickPosition: left');
+      } else {
+        // Otherwise, fall back on just keyboard movement controls
+        el.setAttribute('movement-controls', 'controls: keyboard');
+      }
+    },
+});
