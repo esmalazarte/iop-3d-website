56caaf4f30eacdce9d1f5d8e1d6e65765876db18
esmalazarte <esmalazarte@up.edu.ph>
2023-08-01 12:06:16 +0800

    feat: click scroll controls

--- src/content.js
+++ src/content.js
@@ -12,6 +12,7 @@ const colorScheme = {
 }
   
 // Given a URL to a specific Post ID in WordPress, display its content on a plane.
+// Limitations: Only processes text. Images are inconsistent in geometry
 // Sample URL: https://samplewordpress.com/index.php/wp-json/wp/v2/posts/1
 // Attach to an a-entity e.g. <a-entity postview="url: <post url>">
 AFRAME.registerComponent('postview', {
@@ -24,24 +25,83 @@ AFRAME.registerComponent('postview', {
     },
 
     init: async function () {
+        // Fetch post from CMS
         let response = await fetch(this.data.url);
         let post = await response.json();
 
+        // Set entity to plane container
         this.el.setAttribute('geometry', {
             primitive: 'plane',
             height: 'auto',
             width: this.data.width
         });
 
+        // Set background color
         this.el.setAttribute('material', {
             color: this.data.bgColor
         });
 
+        // Set text properties and value (Title + Text-only Content)
         this.el.setAttribute('text', {
             font: this.data.font,
             color: this.data.textColor,
             zOffset: 0.005,
             value: post.title.rendered + '\n' + post.content.rendered.replace(/<[^<>]*>/g, '').replace(/\n{3,}/g, '\n\n'),
         });
+    },
+});
+
+// Adds scroll controls to the post view i.e. click arrow buttons to scroll
+// IMPORTANT: Previous sibling must be an entity with the 'postview' component
+// Attach to an a-entity right after to a postview
+AFRAME.registerComponent('scrollcontrols', {
+    init: function () {
+        // Get reference to post viewport
+        let post = this.el.previousElementSibling;
+    
+        // Initialize scroll entities
+        let scrollUp = document.createElement('a-circle');
+        let scrollUpArrow = document.createElement('a-text');
+        let scrollDown = document.createElement('a-circle');
+        let scrollDownArrow = document.createElement('a-text');
+    
+        // Sync values to postview
+        let xoffset = post.getAttribute('postview').width / 2 + 1;
+        let arrowBGcolor = post.getAttribute('postview').bgColor;
+        let arrowcolor = post.getAttribute('postview').textColor;
+    
+        // Scroll button properties (static)
+        scrollUp.setAttribute('radius', '0.5');
+        scrollUp.setAttribute('color', arrowBGcolor);
+        scrollUpArrow.setAttribute('value', '>');
+        scrollUpArrow.setAttribute('color', arrowcolor);
+        scrollUpArrow.setAttribute('position', '-0.06 -0.4 0.005');
+        scrollUpArrow.setAttribute('rotation', '0 0 90');
+        scrollUpArrow.setAttribute('scale', '5 5 5');
+        scrollUp.setAttribute('position', xoffset + ' 0.75 0');
+    
+        scrollDown.setAttribute('radius', '0.5');
+        scrollDown.setAttribute('color', arrowBGcolor);
+        scrollDownArrow.setAttribute('value', '<');
+        scrollDownArrow.setAttribute('color', arrowcolor);
+        scrollDownArrow.setAttribute('position', '-0.06 -0.4 0.005');
+        scrollDownArrow.setAttribute('rotation', '0 0 90');
+        scrollDownArrow.setAttribute('scale', '5 5 5');
+        scrollDown.setAttribute('position', xoffset + ' -0.75 0');
+    
+        // Moves post's y position depending on button click
+        scrollUp.addEventListener('click', function(){
+            post.object3D.position.y -= 1
+        })
+    
+        scrollDown.addEventListener('click', function(){
+            post.object3D.position.y += 1
+        })
+    
+        // Add to scene
+        scrollUp.appendChild(scrollUpArrow);
+        scrollDown.appendChild(scrollDownArrow);
+        this.el.appendChild(scrollUp);
+        this.el.appendChild(scrollDown);
     },
 });
