611ed00f5b144b531531679ce688fd45e76b17b6
jstarita <jcstarita1@up.edu.ph>
2023-08-01 13:18:15 +0800

    Add the superhands library (and physics).

--- src/navigation.js
+++ src/navigation.js
@@ -114,3 +114,17 @@ AFRAME.registerComponent('overlay', {
     el.components.material.material.depthTest = false;
   }
 });
+
+AFRAME.registerComponent('phase-shift', {
+  init: function () {
+    var el = this.el
+
+    el.addEventListener('gripdown', function () {
+      el.setAttribute('collision-filter', {collisionForces: true})
+    })
+    
+    el.addEventListener('gripup', function () {
+      el.setAttribute('collision-filter', {collisionForces: false})
+    })
+  }
+});
