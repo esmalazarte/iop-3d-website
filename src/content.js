3363be1f233fa2381099cb6fa5f24da6dee7ded1
esmalazarte <esmalazarte@up.edu.ph>
2023-08-02 16:28:38 +0800

    feat: post selector and other UX improvements

--- src/content.js
+++ src/content.js
@@ -14,7 +14,7 @@ const colorScheme = {
 // Given a URL to a specific Post ID in WordPress, display its content on a plane.
 // IMPORTANT: Must be a child of a "container" e.g. a background screen or plane
 // Limitations: Only processes text. Images are inconsistent in geometry
-// Sample URL: https://samplewordpress.com/index.php/wp-json/wp/v2/posts/1
+// Sample URL: https://example.com/index.php/wp-json/wp/v2/posts/1
 // Attach to an a-entity e.g. <a-entity postview="url: <post url>">
 AFRAME.registerComponent('postview', {
     schema: {
@@ -49,56 +49,198 @@ AFRAME.registerComponent('postview', {
 });
 
 // Adds scroll controls to the post view i.e. click arrow buttons to scroll
-// IMPORTANT: Previous sibling must be an entity with the 'postview' component
-// Attach to an a-entity right after to a postview
+// Attach to an a-entity and set target ID to a postview or listposts component
 AFRAME.registerComponent('scrollcontrols', {
+    schema: {
+        targetID: {type: 'string'},
+        bgColor: {default: colorScheme.darkgray},
+        textColor: {default: colorScheme.offwhite},
+    },
+
     init: function () {
-        // Get reference to post viewport
-        let post = this.el.previousElementSibling;
-    
+        let data = this.data;
+
+        // Get reference to target viewport
+        let target = document.querySelector(this.data.targetID);
+
         // Initialize scroll entities
         let scrollUp = document.createElement('a-circle');
         let scrollUpArrow = document.createElement('a-text');
         let scrollDown = document.createElement('a-circle');
         let scrollDownArrow = document.createElement('a-text');
-    
-        // Sync values to postview
-        let xoffset = post.getAttribute('postview').width / 2 + 1;
-        let arrowBGcolor = post.getAttribute('postview').bgColor;
-        let arrowcolor = post.getAttribute('postview').textColor;
-    
+
         // Scroll button properties (static)
-        scrollUp.setAttribute('radius', '0.5');
-        scrollUp.setAttribute('color', arrowBGcolor);
+        scrollUp.setAttribute('radius', '0.25');
+        scrollUp.setAttribute('color', this.data.bgColor);
+        scrollUp.setAttribute('position', '-0.5 0 0');
         scrollUpArrow.setAttribute('value', '>');
-        scrollUpArrow.setAttribute('color', arrowcolor);
-        scrollUpArrow.setAttribute('position', '-0.06 -0.4 0.005');
+        scrollUpArrow.setAttribute('color', this.data.textColor);
+        scrollUpArrow.setAttribute('position', '-0.275 -0.1 0.005');
         scrollUpArrow.setAttribute('rotation', '0 0 90');
-        scrollUpArrow.setAttribute('scale', '5 5 5');
-        scrollUp.setAttribute('position', xoffset + ' 0.75 0');
-    
-        scrollDown.setAttribute('radius', '0.5');
-        scrollDown.setAttribute('color', arrowBGcolor);
+        scrollUpArrow.setAttribute('scale', '2.5 2.5 2.5');
+        scrollUpArrow.setAttribute('font', 'mozillavr')
+
+        scrollDown.setAttribute('radius', '0.25');
+        scrollDown.setAttribute('color', this.data.bgColor);
+        scrollDown.setAttribute('position', '0.5 0 0');
         scrollDownArrow.setAttribute('value', '<');
-        scrollDownArrow.setAttribute('color', arrowcolor);
-        scrollDownArrow.setAttribute('position', '-0.06 -0.4 0.005');
+        scrollDownArrow.setAttribute('color', this.data.textColor);
+        scrollDownArrow.setAttribute('position', '-0.275 -0.1 0.005');
         scrollDownArrow.setAttribute('rotation', '0 0 90');
-        scrollDownArrow.setAttribute('scale', '5 5 5');
-        scrollDown.setAttribute('position', xoffset + ' -0.75 0');
-    
-        // Moves post's y position depending on button click
+        scrollDownArrow.setAttribute('scale', '2.5 2.5 2.5');
+        scrollDownArrow.setAttribute('font', 'mozillavr')
+        
+        // Moves target's y position depending on button click
         scrollUp.addEventListener('click', function(){
-            post.object3D.position.y -= 1
-        })
-    
+            target.object3D.position.y -= 1
+        });
+
         scrollDown.addEventListener('click', function(){
-            post.object3D.position.y += 1
-        })
-    
+            target.object3D.position.y += 1
+        });
+
+        // Change button color on hover
+        scrollUp.addEventListener('mouseenter', function() {
+            scrollUp.setAttribute('material', {
+                color: data.textColor
+            });
+            scrollUpArrow.setAttribute('text', {
+                color: data.bgColor
+            });
+        });
+
+        scrollDown.addEventListener('mouseenter', function() {
+            scrollDown.setAttribute('material', {
+                color: data.textColor
+            });
+            scrollDownArrow.setAttribute('text', {
+                color: data.bgColor
+            });
+        });
+
+        scrollUp.addEventListener('mouseleave', function() {
+            scrollUp.setAttribute('material', {
+                color: data.bgColor
+            });
+            scrollUpArrow.setAttribute('text', {
+                color: data.textColor
+            });
+        });
+
+        scrollDown.addEventListener('mouseleave', function() {
+            scrollDown.setAttribute('material', {
+                color: data.bgColor
+            });
+            scrollDownArrow.setAttribute('text', {
+                color: data.textColor
+            });
+        });
+
         // Add to scene
         scrollUp.appendChild(scrollUpArrow);
         scrollDown.appendChild(scrollDownArrow);
         this.el.appendChild(scrollUp);
         this.el.appendChild(scrollDown);
     },
+});
+
+// Lists the Titles of every Post in the given URL. User can click a title card to view the post in the postview component
+// Sample URL: https://example.com/index.php/wp-json/wp/v2/posts?categories=1
+// IMPORTANT: Must be a child of a "container" e.g. a background screen or plane
+// Attach to an a-entity e.g. <a-entity listposts="target: #id; url: <post url with category filter>">
+// Categories (IOP WordPress):
+// 1 - Uncategorized
+// 3 - News
+// 4 - Projects
+// 5 - Publications
+AFRAME.registerComponent('listposts', {
+    schema: {
+        url: {type: 'string', default: ''},
+        cardHeight: {type: 'int', default: 1},
+        cardWidth: {type: 'int', default: 2},
+        wrapCount: {type: 'int', default: 20},
+        bgColor: {default: colorScheme.darkgray},
+        textColor: {default: colorScheme.offwhite},
+        font: {default: 'sourcecodepro'},
+        targetID: {type: 'string'}
+    },
+
+    init: async function () {
+        let data = this.data;
+
+        // Fetch all posts from URL
+        let response = await fetch(this.data.url);
+        let posts = await response.json();
+
+        // Prepare variables and references
+        let postView = document.querySelector(this.data.targetID);
+        let container = this.el.parentElement;
+        let yPos = container.getAttribute('geometry').height / 2.5;
+        let postTitle;
+        let titleText;
+
+        // Create element for each post in category
+        for (let i=0; i<posts.length; i++){
+            postTitle = document.createElement('a-entity');
+
+            // Trim title if too long
+            titleText = posts[i].title.rendered;
+            if (titleText.length > this.data.wrapCount * 2 - 5) {
+                titleText = titleText.slice(0, this.data.wrapCount * 2 - 5) + '...';
+            }
+
+            // Set attributes of Title Card and Text
+            postTitle.setAttribute('geometry', {
+                primitive: 'plane',
+                height: this.data.cardHeight,
+                width: this.data.cardWidth
+            });
+
+            postTitle.setAttribute('material', {
+                color: this.data.bgColor
+            })
+
+            postTitle.setAttribute('text', {
+                value: titleText,
+                color: this.data.textColor,
+                wrapCount: this.data.wrapCount,
+                font: this.data.font,
+                xOffset: 0.1,
+                zOffset: 0.005,
+            })
+
+            // Position title cards downwards
+            postTitle.object3D.position.y = yPos;
+            yPos -= (this.data.cardHeight + 0.05);
+
+            // Change post content when clicked on title card
+            postTitle.addEventListener('click', function() {
+                postView.setAttribute('text', {
+                    value: posts[i].title.rendered + '\n' + posts[i].content.rendered.replace(/<[^<>]*>/g, '').replace(/\n{3,}/g, '\n\n')
+                })
+                postView.object3D.position.y = postView.parentElement.getAttribute('geometry').height / 2.25
+            })
+
+            // Change colors on hover
+            postTitle.addEventListener('mouseenter', function() {
+                this.setAttribute('material', {
+                    color: data.textColor
+                })
+                this.setAttribute('text', {
+                    color: data.bgColor
+                })
+            })
+
+            postTitle.addEventListener('mouseleave', function() {
+                this.setAttribute('material', {
+                    color: data.bgColor
+                })
+                this.setAttribute('text', {
+                    color: data.textColor
+                })
+            })
+
+            this.el.appendChild(postTitle);
+        }
+    }
 });
